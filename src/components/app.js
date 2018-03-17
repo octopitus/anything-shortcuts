694d17608ffc65b680220cd648bc657933483747
Luy Tran <octopitus@gmail.com>
2018-03-17 10:24:29 +0700

    Add Flow

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,25 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
+import { h, Component } from 'preact'
+import { Router } from 'preact-router'
 
-import Header from './header';
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Header from './header'
+import Home from '../routes/home'
+import Profile from '../routes/profile'
 // import Home from 'async!../routes/home';
 // import Profile from 'async!../routes/profile';
 
 if (module.hot) {
-	require('preact/debug');
+	require('preact/debug')
 }
 
 export default class App extends Component {
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
 	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+		this.currentUrl = e.url
+	}
 
 	render() {
 		return (
@@ -30,6 +31,6 @@ export default class App extends Component {
 					<Profile path="/profile/:user" />
 				</Router>
 			</div>
-		);
+		)
 	}
 }
--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,47 +1,55 @@
-import { h, Component } from 'preact';
-import style from './style';
+// @flow
+import React, { Component } from 'preact'
+import style from './style.css'
 
-export default class Profile extends Component {
+type Props = {
+	user: string
+}
+
+type State = {
+	time: number,
+	count: number
+}
+
+export default class Profile extends Component<Props, State> {
 	state = {
 		time: Date.now(),
 		count: 10
-	};
+	}
+
+	_timer: any
 
 	// gets called when this route is navigated to
 	componentDidMount() {
 		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
+		this._timer = setInterval(this.updateTime, 1000)
 	}
 
 	// gets called just before navigating away from the route
 	componentWillUnmount() {
-		clearInterval(this.timer);
+		clearInterval(this._timer)
 	}
 
 	// update the current time
 	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
+		this.setState({ time: Date.now() })
+	}
 
 	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
+		this.setState({ count: this.state.count + 1 })
+	}
 
 	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
+	render({ user }: Props, { time, count }: State) {
 		return (
 			<div class={style.profile}>
 				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
-
+				<p>This is the user profile for a user named {user}.</p>
 				<div>Current time: {new Date(time).toLocaleString()}</div>
-
 				<p>
-					<button onClick={this.increment}>Click Me</button>
-					{' '}
-					Clicked {count} times.
+					<button onClick={this.increment}>Click Me</button> Clicked {count} times.
 				</p>
 			</div>
-		);
+		)
 	}
 }
